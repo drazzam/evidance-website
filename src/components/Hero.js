17bcbd60da6531680f3bfad73f4fdbc1af7c16a7
Ahmed Y. Azzam <109034951+drazzam@users.noreply.github.com>
2025-08-29 07:23:17 +0300

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import Hero from '../components/Hero';
-import './Home.css';
+import './Hero.css';
 
 const Home = () => {
   const [visible, setVisible] = useState({});
