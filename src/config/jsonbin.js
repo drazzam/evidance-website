b56fea7627159e6df88b94a4e9c9020aea7c8580
Ahmed Y. Azzam <109034951+drazzam@users.noreply.github.com>
2026-01-18 01:23:13 +0200

    Delete src directory

--- src/config/jsonbin.js
+++
@@ -1,9 +0,0 @@
-// JSONBin configuration
-const JSONBIN_CONFIG = {
-  BIN_ID: '68b175b643b1c97be92f274d',
-  MASTER_KEY: '$2a$10$WWXSel9VjGXEalWwyvd2P.t/EYY8DpBCWilIR1zqhYkVEFqt.1R4y',
-  ACCESS_KEY: '$2a$10$fo2nyFgpKh.7vGOagvtbNuy2pSUNpDjClzarA1EVdylXC6Gk6FC1C',
-  BASE_URL: 'https://api.jsonbin.io/v3'
-};
-
-export default JSONBIN_CONFIG;
